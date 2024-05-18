0333021df73c96ee7267952a19a83b98d6418d24
Gowrinath ragannagari venkata lakshmi <gowrinath.rvl@gmail.com>
2024-05-18 14:36:33 -0400

    minor fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { AuthComponent } from './welcome/auth/auth.component';
-import { LocalComponent } from './local/local.component';
-import { TestComponent } from './src/app/welcome/auth/test/test.component';
-import { InsideTestComponent } from './welcome/auth/test/inside-test/inside-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    AuthComponent,
-    LocalComponent,
-    TestComponent,
-    InsideTestComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
