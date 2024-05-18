138eae1566fd8153f8ce1a4e407cc88191267631
Gowrinath ragannagari venkata lakshmi <gowrinath.rvl@gmail.com>
2024-05-18 14:33:50 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AuthComponent } from './welcome/auth/auth.component';
+import { LocalComponent } from './local/local.component';
+import { TestComponent } from './src/app/welcome/auth/test/test.component';
+import { InsideTestComponent } from './welcome/auth/test/inside-test/inside-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AuthComponent,
+    LocalComponent,
+    TestComponent,
+    InsideTestComponent
   ],
   imports: [
     BrowserModule,
